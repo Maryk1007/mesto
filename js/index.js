f564fb8bc3b41e5dfc300d6cfbfab16d8452bcf6
Maria <mary-sberbank@yandex.ru>
2022-02-02 14:13:29 +0000

    испраить замечания ревьюера

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
-let cardsButton = document.querySelectorAll('.cards__button');
-let cardsButtonLike = document.querySelector('.cards__button_like');
-
-for (let i = 0; i < cardsButton.length; ++i ) {
-  cardsButton[i].addEventListener('click', function(evt) {
-    this.classList.toggle('cards__button_like');
-  });
-};
-
-
 let buttonChange = document.querySelector('.profile__button-change');
 let popup = document.querySelector('.popup')
 let popupOpen = document.querySelector('.popup_opened');
 let buttonClose = document.querySelector('.popup__button-close');
 
+let formPopup = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__info_name');
+let jobInput = document.querySelector('.popup__info_description');
+let profileName = document.querySelector('.profile__name');
+let profileSelfDescription = document.querySelector('.profile__self-description');
+let popupButtonSave = document.querySelector('.popup__button-save');
+
 function openPopup() {
-  popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileSelfDescription.textContent;
 };
 
 buttonChange.addEventListener('click', openPopup);
@@ -26,20 +25,14 @@ function closePopup() {
 
 buttonClose.addEventListener('click', closePopup);
 
-let formPopup = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__description');
-let profileName = document.querySelector('.profile__name');
-let profileSelfDescription = document.querySelector('.profile__self-description');
-let popupButtonSave = document.querySelector('.popup__button-save');
-
 function formSubmit(evt) {
 
   evt.preventDefault();
 
   profileName.textContent = nameInput.value;
   profileSelfDescription.textContent = jobInput.value;
+
+  closePopup();
 };
 
 formPopup.addEventListener('submit', formSubmit);
-popupButtonSave.addEventListener('click', closePopup);
