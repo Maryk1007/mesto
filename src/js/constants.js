5990e1eb272da14d73cb766ed97b2afafca48360
Maria <mary-sberbank@yandex.ru>
2022-05-15 18:25:20 +0300

    сверстать попапы и иконку изменения аватара

--- src/js/constants.js
+++ src/js/constants.js
@@ -40,6 +40,9 @@ export const validationElements = {
 //   }
 // ];
 
+export const url = 'https://mesto.nomoreparties.co/v1/cohort-40';
+export const token = '727072e6-c5f4-4d87-823d-11ff5d188e34';
+
 export const cardsContainer = '.images__list';
 export const buttonChange = document.querySelector('.profile__button-change');
 export const buttonAddPhoto = document.querySelector('.profile__button-addphoto');
@@ -54,5 +57,10 @@ export const formPopupProfile = popupProfile.querySelector('.popup__form');
 export const nameInputProfile = popupProfile.querySelector('.form__input_field_name');
 export const jobInputProfile = popupProfile.querySelector('.form__input_field_description');
 
-export const url = 'https://mesto.nomoreparties.co/v1/cohort-40';
-export const token = '727072e6-c5f4-4d87-823d-11ff5d188e34';
+const popupEditAvatar = document.querySelector('.popup_edit-avatar');
+export const formPopupEditAvatar = popupEditAvatar.querySelector('.popup__form');
+
+const popupConfirmDeletePhoto = document.querySelector('.popup_confirm-delete');
+export const formDeletePhoto = popupConfirmDeletePhoto.querySelector('.popup__form');
+
+
